a009a938cd684595550e1dc936745537ec1719ca
Justin-Ting-VSC <24041609@myrp.edu.sg>
2025-11-03 12:55:26 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -2,25 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 function Header() {
-    return <h1>Verryn's Pizza Co.</h1>
+    return <h1 style={{color: "orange" , fontSize: "48px"}}>Verryn's Pizza Co.</h1>
 }
 
-function Pizza() {
-    return <div>
-        <img src="pizzas/spinach.jpeg" alt="Spinach Pizza" height={100}/>
+function App() {
+    return 
+    <div>
+        <img src="pizzas/spinaci.jpg" alt="Spinach Pizza" height={100}/>
         <h1>Spinach Pizza</h1>
         <p>Tomato, Mozarella, Spinach and Ricotta Cheese</p>
         <p>10</p>
     </div>
-        
 }
-function App() {
-    return <div>
-        <Header/>
-        <Pizza/>
-        
-    </div>
-}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>)
