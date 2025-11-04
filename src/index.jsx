fc747d9c9c6781778a6c522cfc508e70f6fe08ed
Justin-Ting-VSC <24041609@myrp.edu.sg>
2025-11-04 11:07:58 +0800

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+import "./index.css"
 
 function Header() {
-    return <h1 style={{color: "orange" , fontSize: "48px"}}>Verryn's Pizza Co.</h1>
+    return <h1 style={{color: "orange", fontSize: "48px", textTransform : "uppercase"}}>Justin Pizza Co.</h1>
+}
+
+function Pizza(props) {
+    return(
+    <div className="pizza">
+        <img src={props.img}/>
+        <h3>{props.name}</h3>
+        <p>{props.description}</p>
+        <p>{props.price}</p>
+    </div>
+    )    
+}
+
+function Menu() {
+    return(
+    <div className="menu">
+        <h2>Our Menu</h2>
+        <Pizza img="pizzas/spinaci.jpg" name="Spinach Pizza" description="This scrumptious spinach pizza is crispy on the edges and loaded with juicy garlicky spinach, creamy mozzarella and tangy feta cheese. It’s fresh, full of flavor, and absolutely delicious!" price={400}/>
+        <Pizza img="pizzas/funghi.jpg"name="funghi Pizza" description="A pizza funghi, or better known as a mushroom pizza is a world famous pizza. It doesn't need many ingredients, recipe for 2 pizzas." price={100}/>
+    </div>
+    
+)
+}
+
+function Footer() {
+  const currentHour = new Date().getHours(); // get current hour
+  const isOpen = currentHour >= 10 && currentHour < 22; // open 10:00–21:59
+
+  return (
+    <footer className="footer">
+      <p>{isOpen ? "We’re currently open" : "Sorry, we’re closed"}</p>
+      <p>Open daily from 10:00 AM to 10:00 PM</p>
+    </footer>
+  );
 }
 
 function App() {
-    return 
-    <div>
-        <img src="pizzas/spinaci.jpg" alt="Spinach Pizza" height={100}/>
-        <h1>Spinach Pizza</h1>
-        <p>Tomato, Mozarella, Spinach and Ricotta Cheese</p>
-        <p>10</p>
+    return ( 
+    <div className="container">
+        <Header/>
+        <Menu/>
+        <Footer/>
     </div>
+    )
 }
-
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
